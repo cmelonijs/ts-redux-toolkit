90bba451d652be8c52a5c40e39f3051ca8d72849
cmeloni <carlo.meloni@abinsula.com>
2023-05-18 10:53:12 +0200

    feat: setup material ui theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
+import { createTheme } from "@mui/material/styles"
+import { useMemo } from "react"
+import { themeSettings } from "./theme"
+import { ThemeProvider, CssBaseline } from "@mui/material"
+
 function App() {
+  const theme = useMemo(() => createTheme(themeSettings), [])
 
   return (
-    <>
+    <div className="app">
       <h1>home</h1>
-    </>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        hello
+      </ThemeProvider>
+    </div>
   )
 }
 
